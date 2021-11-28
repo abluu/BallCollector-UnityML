d0d2408c2e97647a17ad156543d2d416aa04f372
able <ableelias@gmail.com>
2021-11-28 21:05:59 +0530

    Second commit
    Added  agent, goal and training area basics

---
+++ Ballcollector/Assets/Scripts/BallCollectorAgent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class BallCollectorAgent : Agent
+{
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        base.CollectObservations(sensor);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        base.Heuristic(actionsOut);
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        base.OnEpisodeBegin();
+    }
+
+}
